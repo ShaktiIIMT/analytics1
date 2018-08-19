637127c183d4cc544b84720d8bdfdd3915e324b9
ShaktiIIMT <shakti.p18047@iimtrichy.ac.in>
2018-08-19 19:51:40 +0530

    19/08

--- file1.R
+++ file1.R
@@ -1,1 +1,11 @@
 mtcars
+git config --global user.email "shakti.p18047@iimtrichy.ac.in"
+git config --global user.name "ShaktiIIMT"
+women
+plot(women)
+install.package('car')
+?women
+?mean
+
+x = 1:100
+y = seq(1, 100, by=2)
